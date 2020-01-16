6926d9f97ac5cd4c15ffa9566a907518d1bf6b0d
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-16 17:07:15 -0300

    Add files via upload

---
+++ age/age.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace age
+{
+    class age
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32 (Console.ReadLine());
+
+            if (age < 18)
+            {
+                Console.WriteLine ("That's too bad! You will have to wait a couple of years.");
+            }
+
+            else if (age == 18)
+            {
+                Console.WriteLine ("Puh, barely made it!");
+            }
+
+            else
+            {
+                Console.WriteLine("You are allowed to enter.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
