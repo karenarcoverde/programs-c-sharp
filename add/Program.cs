0f8fd2b78ee8f06f83a6dbd15bd1551914217e2a
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-16 18:47:23 -0300

    Add files via upload

---
+++ add/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            add(10, 8);
+            add(10, 8, 2);
+            Console.ReadKey();
+        }
+
+        static void add(int num01, int num02)
+        {
+            Console.WriteLine(num01 + num02);
+        }
+
+        static void add (int num01, int num02, int num03)
+        {
+            Console.WriteLine(num01 + num02 + num03);
+        }
+    }
+}
---
+++ resultwithreturn/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace resultwithreturn
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int resultado = add(1, 8);
+            if (resultado > 10)
+            {
+                Console.WriteLine("Resultado é maior que 10!");
+            }
+
+            else
+            {
+                Console.WriteLine("Resultado é menor ou igual a 10!");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int add (int num01, int num02)
+        {
+            return num01 + num02;
+        }
+
+    }
+}
