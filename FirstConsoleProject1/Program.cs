72e67bafc2b911d47725c75b74b3e472dd299d9b
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-16 17:03:59 -0300

    Add files via upload

---
+++ FirstConsoleProject1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstConsoleProject1
+{
+    class Program
+    {
+        static void Main(string[] args) // This is a method called "Main". It is called when the program starts.
+        {
+            Console.WriteLine("Hello, I'm a computer, I'm send from the future.\nThis is a new line.");
+            Console.ReadKey(); // Obtém o próximo caractere ou tecla de função pressionada pelo usuário. A tecla pressionada é exibida na janela do console.
+
+            Console.WriteLine("My name is Karen & I'm 24 years old. <Press space to continue>");
+            Console.ReadKey();
+            Console.WriteLine("\nMy favorite color is pink...");
+            Console.ReadKey();
+
+        }
+    }
+}
