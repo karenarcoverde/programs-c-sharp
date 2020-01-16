2eb02e29c33022e1809dd99480e1df98b3f910f6
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-16 18:46:05 -0300

    Add files via upload

---
+++ metodos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            usuario();
+            Console.ReadKey();
+        }
+
+        static void usuario ()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
