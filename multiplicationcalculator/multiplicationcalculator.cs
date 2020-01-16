1cbce47c5af8ab2819142fc0ae875b793b355489
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-16 17:04:57 -0300

    Add files via upload

---
+++ multiplicationcalculator/multiplicationcalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace multiplicationcalculator
+{
+    class multiplicationcalculator
+    {
+        static void Main(string[] args)
+        {
+            int num01;
+            int num02;
+
+            Console.Write ("Type a number to be multiplied: ");
+            num01 = Convert.ToInt32 (Console.ReadLine());
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Result is " + num01 * num02);
+            Console.ReadKey();
+        }
+    }
+}
