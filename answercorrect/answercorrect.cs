a3f68591ef2b517b7c9affcba63f701f07029d90
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-16 17:06:36 -0300

    Add files via upload

---
+++ answercorrect/answercorrect.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace answercorrect
+{
+    class answercorrect
+    {
+        static void Main(string[] args)
+        {
+            Random numberGenerator = new Random();
+
+            int num01 = numberGenerator.Next(1, 15);
+            int num02 = numberGenerator.Next(1, 15);
+
+            Console.WriteLine("Quanto é " + num01 + " vezes " + num02 + "?");
+
+            int resposta = Convert.ToInt32(Console.ReadLine());
+
+            if (resposta == num01 * num02)
+            {
+                int respostaIndice = numberGenerator.Next(1, 4);
+
+                switch (respostaIndice)
+                {
+                    case 1:
+                        Console.WriteLine ("Correto. Muito bem!");
+                        break;
+                    case 2:
+                        Console.WriteLine("A resposta está correta.");
+                        break;
+                    default:
+                        Console.WriteLine("Você já tinha praticado?");
+                        break;
+                }
+
+            }
+
+            else
+            {
+                int diferença = Math.Abs(resposta - (num01 * num02)); // pega o valor absoluto da diferença
+
+                if (diferença == 1)
+                {
+                    Console.WriteLine("Bem perto do resultado!");
+                }
+
+                else if (diferença <= 10)
+                {
+                    Console.WriteLine("Você pode fazer melhor do que isso");
+                }
+
+                else
+                {
+                    Console.WriteLine("Você está bem longe do resultado");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
