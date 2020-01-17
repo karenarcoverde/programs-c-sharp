ac6f398c18384cef620fe438523b7bed805f8893
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-16 23:32:11 -0300

    Add files via upload

---
+++ names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[3]
+            {
+                "Karen", "Erick", "Lourdes"
+            };
+
+
+            foreach (string nome in nomes)
+            {
+                Console.Write(nome + ",");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ numeros/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int [5];
+            numbers[0] = 12;
+            numbers[1] = 3;
+            numbers[2] = 5;
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number + ",");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
