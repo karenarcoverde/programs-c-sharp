0f6dabd096ec40a0fb180bcc16b0bc739b00f84f
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-24 01:14:09 -0300

    Add files via upload

---
+++ animal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace animal
+{
+    class Animal
+    {
+        public static int Count = 0;
+
+        public string nome = "Spotty";
+        public int idade = 6;
+        public float felicidade = 0.5f; 
+
+        public void Print()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("felicidade: " + felicidade);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Animal cachorro = new Animal();
+            Console.WriteLine(cachorro.nome);
+            cachorro.nome = "Tom";
+            Console.WriteLine(cachorro.nome);
+
+            cachorro.Print();
+
+            Animal.Count += 2;
+            Animal gato = new Animal();
+            Console.WriteLine(Animal.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ animal2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace animal2
+{
+    class Animal
+    {
+        // CLASS VARIABLES
+        public static int Contador = 0;
+
+        public string nome;
+        public int idade;
+        public float felicidade;
+
+        // CLASS CONSTRUCTORS
+        public Animal ()
+        {
+            nome = "Spotty";
+            idade = 6;
+            felicidade = 0.5f;
+
+            Contador++;
+        }
+
+        public Animal (string _nome, int _idade, float _felicidade)
+        {
+            nome = _nome;
+            idade = _idade;
+            felicidade = _felicidade;
+
+            Contador++;
+        }
+        // CLASS METHODS
+        public void Print ()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("Felicidade: " + felicidade);
+        }
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Animal cachorro = new Animal();
+           cachorro.Print();
+
+            Console.WriteLine();
+
+            Animal gato = new Animal("Tomzinho",10, 0.8f);
+            gato.Print();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Numero de animais:" + Animal.Contador);
+
+           Console.ReadKey();
+        }
+    }
+}
