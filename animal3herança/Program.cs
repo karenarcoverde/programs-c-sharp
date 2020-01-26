a98d22fd91ab7ce03105612b916ab34f50d6cb44
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-26 18:08:54 -0300

    Add files via upload

---
+++ animal3herança/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace animal3herança
+{
+    class Program
+    {
+        class Animal
+        {
+            public string nome;
+            public int idade;
+            public float felicidade;
+
+            public void PrintBase()
+            {
+                Console.WriteLine("Nome: " + nome);
+                Console.WriteLine("Idade: " + idade);
+                Console.WriteLine("Felicidade: " + felicidade);
+            }
+        }
+
+        class cachorro : Animal
+        {
+            public int ContadorLocal;
+
+            public void Latido ()
+            {
+                Console.WriteLine("Wuf!");
+                base.felicidade += 0.1f;
+
+            }
+        }
+        class gato : Animal
+        {
+            public float fofura;
+
+            public void Miado()
+            {
+                Console.WriteLine("Meow!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            cachorro Spotty = new cachorro();
+            Spotty.nome = "Spotty";
+            Spotty.idade = 4;
+            Spotty.felicidade = 0.8f;
+            Spotty.ContadorLocal = 25;
+            Spotty.PrintBase();
+            Spotty.Latido();
+            Console.WriteLine("Nova felicidade: " + Spotty.felicidade);
+
+            Console.WriteLine();
+
+            gato heisenberg = new gato();
+            heisenberg.nome = "Heisenberg";
+            heisenberg.idade = 13;
+            heisenberg.felicidade = 0.3f;
+            heisenberg.fofura = 0.4f;
+            heisenberg.PrintBase();
+            heisenberg.Miado();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ enum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        enum Raça {Bulldog, Boxer, Chihuahua, Beagle};
+
+        class Animal
+        {
+             public string nome;
+             public int idade;
+             public float felicidade;
+        }
+
+        class cachorro : Animal
+        {
+            public Raça raça;
+
+            public cachorro (string _nome, int _idade, float _felicidade, Raça _raça)
+            {
+                nome = _nome;
+                idade = _idade;
+                felicidade = _felicidade;
+
+                raça = _raça;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Nome: " + nome);
+                Console.WriteLine("Idade: " + idade);
+                Console.WriteLine("Felicidade: " + felicidade);
+                Console.WriteLine("Raça: " + raça);
+            }
+
+         }
+             static void Main(string[] args)
+             {
+                 cachorro Hulk = new cachorro("Hulk", 6, 0.7f, Raça.Chihuahua);
+                 Hulk.Print();
+
+                 Console.ReadKey();
+             }
+    }
+}
