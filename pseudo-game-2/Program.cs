80d3d35e88a5acbf91a169c9f139362e4235ff95
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-27 19:31:16 -0300

    Add files via upload

---
+++ pseudo-game-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pseudo_game_2
+{
+    class Program
+    {
+        class Jogador
+        {
+            private int _saude = 100;
+
+            public int saude
+            {
+                get
+                {
+                    return _saude;
+                }
+            }
+
+            public void dano(int _dno)
+            {
+                _saude -= _dno;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Jogador Karen = new Jogador();
+            Karen.dano(30);
+            Console.WriteLine(Karen.saude);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pseudo-game/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pseudo_game
+{
+    class Program
+    {
+        class Jogador
+        {
+            public int saude = 100;
+            
+            public void dano (int _dno)
+            {
+                saude -= _dno;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Jogador Karen = new Jogador();
+            Console.WriteLine(Karen.saude);
+            Karen.saude = 40;
+            Console.WriteLine(Karen.saude);
+
+            Karen.dano(50);
+            Console.WriteLine(Karen.saude);
+
+            Console.ReadKey();
+
+        }
+    }
+}
