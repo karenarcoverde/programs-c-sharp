178a38cef13b409f90bf7f18141b9ec6280cf1d9
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-27 19:29:39 -0300

    Add files via upload

---
+++ date/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime curTime = DateTime.Now;
+            Console.WriteLine(curTime);
+
+            Console.ReadKey();
+        }
+    }
+}
