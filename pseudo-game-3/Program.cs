ec960817e9a2d0327d44e87cf8122d9b37639f34
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-01-27 19:35:14 -0300

    Add files via upload

---
+++ pseudo-game-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pseudo_game_3
+{
+    class Program
+    {
+        class Jogador
+        {
+            private int _saude = 100;
+            public int saude
+            {
+                get
+                {
+                    return _saude;
+                }
+                set
+                {
+                    _saude = value;
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Jogador Karen = new Jogador();
+            Console.WriteLine(Karen.saude);
+            Karen.saude = 20;
+            Console.WriteLine(Karen.saude);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pseudo-game-4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace pseudo_game_3
+{
+    class Program
+    {
+        class Jogador
+        {
+            private int _saude = 100;
+            public int saude
+            {
+                get
+                {
+                    return _saude;
+                }
+                set
+                {
+                    if (value <= 0)
+                    {
+                        _saude = 0;
+                    }
+
+                    else if (value >= 100)
+                    {
+                        _saude = 100;
+                    }
+
+                    else
+                    {
+                        _saude = value;
+                    }
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Jogador Karen = new Jogador();
+            Console.WriteLine(Karen.saude);
+            Karen.saude -= 200;
+            Console.WriteLine(Karen.saude);
+            Karen.saude += 400;
+            Console.WriteLine(Karen.saude);
+            Karen.saude = 50;
+            Console.WriteLine(Karen.saude);
+
+            Console.ReadKey();
+        }
+    }
+}
