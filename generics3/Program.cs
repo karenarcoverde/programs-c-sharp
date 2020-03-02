3af4a2e76f201794ec48b0129904e6c805b30f33
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-03-02 20:45:07 -0300

    Add files via upload

---
+++ generics3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace generics3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine (Utilitario.CompararValor (10,3));
+            Console.WriteLine (Utilitario.CompararTipos("hello", "world"));
+
+            Console.ReadKey();
+        }
+
+        class Utilitario
+        {
+            public static bool CompararValor<T01, T02>(T01 valor01, T02 valor02)
+            {
+                return valor01.Equals(valor02);
+            }
+
+            public static bool CompararTipos <T01,T02> (T01 tipo01, T02 tipo02)
+            {
+                return typeof(T01).Equals(typeof(T02));
+            }
+        }
+    }
+}
