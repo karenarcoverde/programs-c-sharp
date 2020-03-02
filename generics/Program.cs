bfa0ae35d913bb9db1feda9fb4f4e55e3db99804
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-03-02 20:43:08 -0300

    Add files via upload

---
+++ generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ChaveValorPar<string, int> meaning = new ChaveValorPar<string, int>("Vida", 42);
+            Console.ReadKey();
+            meaning.Print();
+
+        }
+    }
+
+    class ChaveValorPar <TChave, TValor>
+    {
+        public TChave chave;
+        public TValor valor;
+
+        public ChaveValorPar (TChave _chave, TValor _valor)
+        {
+            chave = _chave;
+            valor = _valor;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Chave: " + chave.ToString());
+            Console.WriteLine("Valor: " + valor.ToString());
+        }
+
+    }
+}
