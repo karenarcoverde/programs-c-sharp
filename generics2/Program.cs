7a4ef2010f4e5cbbfc847034809459162df7d836
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-03-02 20:43:56 -0300

    Add files via upload

---
+++ generics2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generics2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> precos = new Dictionary<string, int>(5);
+            precos.Add("Melancia", 5);
+            precos.Add("Carro", 100000);
+            Console.WriteLine(precos["Melancia"]);
+
+            Console.ReadKey();
+        }
+    }
+}
